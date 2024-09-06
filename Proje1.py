58c81fe8ad43521cf3c01c76ef1b4da707b82fd6
Tolga Akgol <tolgaakgol@outlook.com>
2024-09-07 01:25:03 +0300

    :q

---
+++ Proje1.py
@@ -0,0 +1,12 @@
+def flatten_list(l):
+    flattened_list = []
+    def flatten(l): # List eleman kalmayana kadar recursive bir fonksiyon donecek
+        for e in l:
+            if isinstance(e, list): # List olanlar bir daha flatten fonksiyonuna girecek
+                flatten(e)
+            else:
+                flattened_list.append(e)
+    flatten(l)
+    return flattened_list
+l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5] # Sample list
+print(flatten_list(l))
